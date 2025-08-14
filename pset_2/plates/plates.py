b545e95a8b64c648a5a9a13c519235981f7ba0bc
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2025-08-14 21:38:52 +0200

    Vanity Plates Problem Set

---
+++ pset_2/plates/plates.py
@@ -0,0 +1,35 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    return two_letters(s) and correct_length(s) and numbers_and_letters(s) and numbers_end(s)
+
+def two_letters(s):
+    return 2 <= len(s) and s[0].isalpha() and s[1].isalpha()
+
+def correct_length(s):
+    return 2 <= len(s) <= 6
+
+def numbers_and_letters(s):
+    return s.isalnum()
+
+def numbers_end(s):
+    number_start = False
+
+    for char in s:
+        if char.isdigit():
+            if not number_start:
+                number_start = True
+                if char == "0":
+                    return False
+        elif number_start:
+            return False
+    return True
+
+if __name__ == "__main__":
+    main()
